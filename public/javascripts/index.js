0afa14d8585685956235ada6671eb5b6821178c5
amr97 <aa838@snu.edu.in>
2018-04-25 01:28:03 +0530

    Commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-  var timeData = [],
-    temperatureData = [],
-    humidityData = [];
-
+  var temperatureData = [];
+  var coordinateData = [];
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myCanvas").getContext("2d");
 
@@ -12,12 +10,46 @@ $(document).ready(function () {
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
   }
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYW1yOTciLCJhIjoiY2pnZGNtZTZiMDdhNTJ4cWFheHo2MTcxbSJ9.HbYDR0RfFKdul5e7xIAunA';
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [77.575947,28.526013],
+      zoom: 17
+  });
+  map.on('load', function () {
+      map.addLayer({
+          "id": "route",
+          "type": "line",
+          "source": {
+              "type": "geojson",
+              "data": {
+                  "type": "Feature",
+                  "properties": {},
+                  "geometry": {
+                      "type": "LineString",
+                      "coordinates": coordinateData
+                  }
+              }
+          },
+          "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#888",
+              "line-width": 8
+          }
+      });
+  });
   ws.onmessage = function (message) {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
+      if(!obj.temperature || !obj.coordinates)
+          return;
       temperatureData.push(obj.temperature);
-
+      coordinateData.push(obj.coordinates);
       draw(obj.temperature);
     } catch (err) {
       console.error(err);
